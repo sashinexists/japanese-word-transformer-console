b5bcf5f28d39b4d7c9453ee32cc302638f2581ff
sashin <myself@sashinexists.com>
2021-05-13 10:11:59 +1000

    list of joshi serialised from file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,88 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use serde::Deserializer;
+use serde_json::json;
+use serde_json::Value as JsonValue;
+
 use regex::Regex;
-use std::{fmt, io, str};
-use serde_json;
+use std::{
+    fmt, fs, io,
+    str::{self, FromStr},
+};
 
-
+#[derive(Serialize, Deserialize, Debug)]
 struct Joshi {
     name: String,
     function: String,
     joins_with: Vec<WordType>,
     godan_stem: GodanStem,
     ichidan_form: String,
-    word_type: WordType
+    word_type: WordType,
 }
 
-
-
 fn main() {
-    //categorise_verbs(vec!["行く","来る","食べる","歌う","起きる","話す","する","分かる","答える","くる" ])
-
     //let example_verbs = vec!["買う", "聞く","話す","持つ","死ぬ","飛ぶ","飲む","取る"];
+    //categorise_verbs(example_verbs);
 
+    const JOSHI_DATA_FILE_PATH: &str = "src/joshi.json";
+
+    let joshi_data =
+        fs::read_to_string(JOSHI_DATA_FILE_PATH).expect("Something went wrong reading the file");
+
+    //println!("With text:\n{}", joshi_data);
+
+    let json_str = r#"{
+        "name": "ます",
+        "function": "Makes it formal",
+        "joins_with": [
+            "Doushi"
+        ],
+        "godan_stem": "i",
+        "ichidan_form": "N/A",
+        "word_type": "Doushi"
+    }"#;
+
+    let joshi_list: Vec<Joshi> = serde_json::from_str(joshi_data.as_str()).unwrap();
+    println!("{:#?}", joshi_list[3]);
+
+    //println!("Is the joshi string valid? {}", validate_joshi_json(json_str));
+
+    /*
+    let res = serde_json::from_str(json_str);
+
+    let mut name = "borked";
+    let mut function = "borked";
+    let mut word_type = "borked";
+    if res.is_ok() {
+        let p: JsonValue = res.unwrap();
+        let name_res = p["name"].as_str();
+        if name_res.is_some() {
+            name = name_res.unwrap();
+        }
+        let word_type_res = p["word_type"].as_str();
+        if word_type_res.is_some() {
+            word_type = word_type_res.unwrap();
+        }
+        let function_res = p["function"].as_str();
+        if function_res.is_some() {
+            function = function_res.unwrap();
+        }
+
+        println!(
+            "The {} joshi is a {} that {}",
+            name, word_type, function
+        );
+    } else {
+        "Sorry could not parse JSON";
+    }*/
+
+    //verb_categoriser_loop();
+}
+
+fn verb_categoriser_loop() {
     let mut input = String::new();
     while input != "出る" {
         input.clear();
@@ -32,26 +96,28 @@ fn main() {
     println!("さようなら");
 }
 
-
+#[derive(Serialize, Deserialize)]
 enum VerbGroup {
     Ichidan,
     Godan,
     Kurusuru,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 enum WordType {
-    meishi,
-    doushi,
-    keiyoushi,
-    joshi,
+    Meishi,
+    Doushi,
+    Keiyoushi,
+    Joshi,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 enum GodanStem {
-    a,
-    i,
-    u,
-    e,
-    o
+    A,
+    I,
+    U,
+    E,
+    O,
 }
 
 impl fmt::Display for VerbGroup {
