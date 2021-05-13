36e1c884b5dce768baa5d9a4f4bd6fa2b3be570d
sashin <myself@sashinexists.com>
2021-05-13 14:24:02 +1000

    Ichidan verbs now working

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::{
     str::{self, FromStr},
 };
 
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Joshi {
     name: String,
@@ -21,86 +22,6 @@ struct Joshi {
     godan_stem: GodanStem,
     ichidan_form: String,
     word_type: WordType,
-}
-
-fn main() {
-    //let example_verbs = vec!["買う", "聞く","話す","持つ","死ぬ","飛ぶ","飲む","取る"];
-    //categorise_verbs(example_verbs);
-
-    const JOSHI_DATA_FILE_PATH: &str = "src/joshi.json";
-
-    let joshi_data =
-        fs::read_to_string(JOSHI_DATA_FILE_PATH).expect("Something went wrong reading the file");
-
-    //println!("With text:\n{}", joshi_data);
-
-    let json_str = r#"{
-        "name": "ます",
-        "function": "Makes it formal",
-        "joins_with": [
-            "Doushi"
-        ],
-        "godan_stem": "i",
-        "ichidan_form": "N/A",
-        "word_type": "Doushi"
-    }"#;
-
-    let joshi_list: Vec<Joshi> = serde_json::from_str(joshi_data.as_str()).unwrap();
-    println!("{:#?}", joshi_list[3]);
-
-    //println!("Is the joshi string valid? {}", validate_joshi_json(json_str));
-
-    /*
-    let res = serde_json::from_str(json_str);
-
-    let mut name = "borked";
-    let mut function = "borked";
-    let mut word_type = "borked";
-    if res.is_ok() {
-        let p: JsonValue = res.unwrap();
-        let name_res = p["name"].as_str();
-        if name_res.is_some() {
-            name = name_res.unwrap();
-        }
-        let word_type_res = p["word_type"].as_str();
-        if word_type_res.is_some() {
-            word_type = word_type_res.unwrap();
-        }
-        let function_res = p["function"].as_str();
-        if function_res.is_some() {
-            function = function_res.unwrap();
-        }
-
-        println!(
-            "The {} joshi is a {} that {}",
-            name, word_type, function
-        );
-    } else {
-        "Sorry could not parse JSON";
-    }*/
-
-    //verb_categoriser_loop();
-}
-
-fn verb_categoriser_loop() {
-    let mut input = String::new();
-    while input != "出る" {
-        input.clear();
-        println!("動詞を入力下さい　(Please enter a verb) | Alternatively, type 出る to exit");
-        io::stdin().read_line(&mut input).unwrap();
-        input.pop();
-        if input != "出る" {
-            categorise_verbs(vec![input.as_str()]);
-        }
-    }
-    println!("さようなら");
-}
-
-#[derive(Serialize, Deserialize)]
-enum VerbGroup {
-    Ichidan,
-    Godan,
-    Kurusuru,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -120,6 +41,108 @@ enum GodanStem {
     O,
 }
 
+#[derive(Serialize, Deserialize)]
+enum VerbGroup {
+    Ichidan,
+    Godan,
+    Kurusuru,
+}
+
+fn main() {
+    //let example_verbs = vec!["買う", "聞く","話す","持つ","死ぬ","飛ぶ","飲む","取る"];
+    //categorise_verbs(example_verbs);
+
+    const JOSHI_DATA_FILE_PATH: &str = "src/joshi.json";
+
+    let joshi_data =
+        fs::read_to_string(JOSHI_DATA_FILE_PATH).expect("Something went wrong reading the file");
+
+    let joshi_list: Vec<Joshi> = serde_json::from_str(joshi_data.as_str()).unwrap();
+
+    verb_transformation_loop(&joshi_list);
+    //println!("{:#?}", joshi_list[3]);
+
+    //verb_categoriser_loop();
+}
+
+fn verb_transformation_loop(joshi_list:&Vec<Joshi>) {
+    let mut input = String::new();
+    while input != "出" {
+        input.clear();
+        println!("\n動詞を入力下さい　(Please enter a verb) | Alternatively, type 出 to exit\n");
+        io::stdin().read_line(&mut input).unwrap();
+        input.pop();
+        let verb_type = categorise_verb(&input);
+        if input != "出" {
+            println!("\n何をしたいですか？ (What would you like to do?)\n");
+            println!("{}", build_transformation_menu(&joshi_list));
+            let mut action = String::new();
+            io::stdin().read_line(&mut action).unwrap();
+            action.pop();
+            let action = action.parse::<usize>().unwrap();
+            println!("The {}-form of {} is {}", joshi_list[action].name, input, transform_verb(&input, &joshi_list[action]) );
+        }
+    }
+    println!("さようなら");
+}
+
+fn build_transformation_menu(joshi_list:&Vec<Joshi>) -> String {
+    let mut output = String::new();
+    for i in 0..joshi_list.len() {
+        output += &format!("{})  {}  | {}\n", i, joshi_list[i].function, joshi_list[i].name);
+    }
+    output
+}
+
+fn transform_verb(verb: &str, joshi:&Joshi) -> String {
+    let verb_group = categorise_verb(verb);
+    match verb_group {
+        VerbGroup::Ichidan => transform_ichidan_verb(verb, joshi),
+        VerbGroup::Godan => transform_godan_verb(verb, joshi),
+        VerbGroup::Kurusuru => transform_kuru_suru(verb, joshi)
+    }
+}
+
+fn transform_ichidan_verb(verb: &str, joshi:&Joshi) -> String{
+    let mut sticky_stem = verb.to_string();
+    sticky_stem.pop();
+    if joshi.ichidan_form=="N/A" {
+        sticky_stem + joshi.name.as_str()
+    } else {
+        sticky_stem + joshi.ichidan_form.as_str()
+    }
+}
+
+fn transform_godan_verb(verb: &str, joshi:&Joshi) -> String {
+    "ITS A GODDAMN GODAN VERB".to_string()
+}
+
+fn get_godan_stem(verb: &str, stem:GodanStem) -> String {
+    "hello".to_string()
+}
+
+
+
+fn transform_kuru_suru(verb: &str, joshi:&Joshi) -> String {
+    "THIS IS KURU OR SURU".to_string()
+}
+
+fn verb_categoriser_loop() {
+    let mut input = String::new();
+    while input != "出る" {
+        input.clear();
+        println!("動詞を入力下さい　(Please enter a verb) | Alternatively, type 出る to exit");
+        io::stdin().read_line(&mut input).unwrap();
+        input.pop();
+        if input != "出る" {
+            categorise_verbs(vec![input.as_str()]);
+        }
+    }
+    println!("さようなら");
+}
+
+
+
 impl fmt::Display for VerbGroup {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
@@ -130,9 +153,7 @@ impl fmt::Display for VerbGroup {
     }
 }
 
-fn get_sticky_stem(verb: &str) -> String {
-    "hello".to_string()
-}
+
 
 fn categorise_verb(verb: &str) -> VerbGroup {
     if is_kuru_or_suru(verb) {
